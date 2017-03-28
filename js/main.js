30a18cb9a5e6fbc874745b5d2573eece92e31c08
nov101 <vistavistavista@mail.ru>
2017-03-28 16:28:54 +0400

    Исправил линковку module5-task1

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@
 var toggle_btn = document.querySelector(".main-nav__toggle");
 var mobile_menu = document.querySelector(".main-nav__list");
 
- if (document.documentElement.clientWidth >= 768) {
+if (document.documentElement.clientWidth >= 768) {
     mobile_menu.classList.remove("main-nav__list--open")
-  }
+}
 
   mobile_menu.classList.remove('main-nav__list--nojs');
 
